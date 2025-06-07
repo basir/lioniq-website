df4f0963598f4e1732121777fa4fd562e9885548
basir <basir.jafarzadeh@gmail.com>
2025-06-07 22:31:03 +0330

    first commit

---
+++ config/site.ts
@@ -0,0 +1,30 @@
+export const siteConfig = {
+  name: "Lioniq",
+  url: "https://www.lioniq.app",
+  getStartedUrl: "https://www.lioniq.app/docs/getting-started/introduction",
+  ogImage: "https://www.lioniq.app/og.jpg",
+  description: "Lioniq is a language learning app power by ai.",
+  links: {
+    twitter: "https://twitter.com/lioniq_app",
+    github: "https://github.com/basir/lioniq",
+    email: "mailto:lioniq.app@gmail.com",
+  },
+  pricing: {
+    pro: "https://lioniq.app/buy/pro",
+    max: "https://lioniq.app/buy/max",
+  },
+  stats: {
+    figma: 3285,
+    github: 793,
+    cli: 6524,
+    total: "10.6k+",
+    updated: "27 May 2025",
+    sections: 72,
+    illustrations: 15,
+    animations: 14,
+    appTemplates: 2,
+    websiteTemplates: 4,
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
